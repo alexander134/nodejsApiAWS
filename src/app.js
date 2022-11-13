704bf4d02714fbe22c87b68afc8862e901097586
alexander134 <alexanderdiaz_134@hotmail.com>
2022-11-13 18:45:51 -0300

    firts

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import {  pool} from "./db.js";
+import { PORT_DBL } from "./config.js";
+const app = express()
+
+app.get('/', (req,res)=>{
+    res.send('welcome to server')
+})
+
+app.get('/ping', async (req,res)=>{
+    const [result] =await pool.query("select * from usu");
+    //console.log(result);
+    res.json(result)
+})
+app.get('/aws', async (req,res)=>{
+    const [result] =await pool.query("SELECT * from product");
+    //console.log(result);
+    res.json(result)
+})
+
+
+app.get('/crear_usuario', async (req,res)=>{
+    const [result] =await pool.query("insert into usu(name)values('alexander4')");
+    //console.log(result);
+    res.json(result[0])
+})
+
+app.listen(PORT_DBL)
+
+console.log('Server on port', PORT_DBL)
---
+++ src/db.js
@@ -0,0 +1,40 @@
+import {createConnection}  from "mysql2/promise";
+import {createPool}  from "mysql2/promise";
+import {USER_DB,HOST_DB,DATABASE,PASS_DB,PORT_DB}  from "./config.js";
+
+
+/*createConnection({
+    user: 'bsale_test',
+    password:'bsale_test',
+    host:'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
+    //port:3306,
+    database:'bsale_test'
+})*/
+
+/*export const conn= await createConnection({
+    user: 'root',
+    password:'narualex',
+    host:'localhost',
+    port:3306,
+    database:'usuariodb'
+})*/
+
+
+/*export const pool= createPool({
+    user: 'root',
+    password:'narualex',
+    host:'localhost',
+    port:3306,
+    database:'usuariodb'
+})*/
+
+
+
+export const pool= createPool({
+    user: USER_DB,
+    password:PASS_DB,
+    host:HOST_DB,
+    port:PORT_DB,
+    database:DATABASE
+})
+
