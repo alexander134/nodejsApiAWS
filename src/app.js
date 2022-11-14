4e3bfcb3c906b4051ecdef29586f9a1100cd3bf4
alexander134 <alexanderdiaz_134@hotmail.com>
2022-11-13 23:51:02 -0300

    correccion de nose y add congig 2 DB

--- src/app.js
+++ src/app.js
@@ -4,33 +4,31 @@ import { PORT_DBL,USER_DB,HOST_DB,DATABASE,PASS_DB,PORT_DB}  from "./config.js";
 const app = express()
 
 app.get('/', async(req,res)=>{
-    console.log("hols /");
-    res.send(` RUTA: / </br> USER_DB:${USER_DB} </br> HOST_DB:${HOST_DB}</br> DATABASE:${DATABASE}</br> PASS_DB:${PASS_DB}</br>PORT_DB:${PORT_DB}</br>`)
-    //const [result] =await pool.query("SELECT * from product");
-    //console.log(result);
-    //res.json(result)
+    const [result] =await pool.query("SELECT * from product");
+    //res.send(` RUTA: / </br> USER_DB:${USER_DB} </br> HOST_DB:${HOST_DB}</br> DATABASE:${DATABASE}</br> PASS_DB:${PASS_DB}</br>PORT_DB:${PORT_DB}</br>`)
+    res.json(result)
 })
 
 
 app.get('/ping', async (req,res)=>{
-    //const [result] =await pool.query("select * from usu");
+    const [result] =await pool.query("SELECT * from product");
     console.log("hols ping");
-    //res.json(result)
-    res.send(` RUTA: ping </br> USER_DB:${USER_DB} </br> HOST_DB:${HOST_DB}</br> DATABASE:${DATABASE}</br> PASS_DB:${PASS_DB}</br>PORT_DB:${PORT_DB}</br>`)
+    res.json(result)
+    //res.send(` RUTA: ping </br> USER_DB:${USER_DB} </br> HOST_DB:${HOST_DB}</br> DATABASE:${DATABASE}</br> PASS_DB:${PASS_DB}</br>PORT_DB:${PORT_DB}</br>`)
 })
 
 app.get('/aws', async (req,res)=>{
-    //const [result] =await pool.query("SELECT * from product");
+    const [result] =await pool.query("SELECT * from product");
     console.log("hols aws");
-    //res.json(result)
-    res.send(` RUTA: aws </br> USER_DB:${USER_DB} </br> HOST_DB:${HOST_DB}</br> DATABASE:${DATABASE}</br> PASS_DB:${PASS_DB}</br>PORT_DB:${PORT_DB}</br>`)
+    res.json(result)
+    //res.send(` RUTA: aws </br> USER_DB:${USER_DB} </br> HOST_DB:${HOST_DB}</br> DATABASE:${DATABASE}</br> PASS_DB:${PASS_DB}</br>PORT_DB:${PORT_DB}</br>`)
 })
 
 
 app.get('/crear_usuario', async (req,res)=>{
     const [result] =await pool.query("SELECT * from product");
     //const [result] =await pool.query("insert into usu(name)values('alexander5')");
-    console.log(` RUTA: crear_usuario </br> USER_DB:${USER_DB} </br> HOST_DB:${HOST_DB}</br> DATABASE:${DATABASE}</br> PASS_DB:${PASS_DB}</br>PORT_DB:${PORT_DB}</br>`);
+    //console.log(` RUTA: crear_usuario </br> USER_DB:${USER_DB} </br> HOST_DB:${HOST_DB}</br> DATABASE:${DATABASE}</br> PASS_DB:${PASS_DB}</br>PORT_DB:${PORT_DB}</br>`);
     res.json(result)
     //res.send(` RUTA: crear_usuario </br> USER_DB:${USER_DB} </br> HOST_DB:${HOST_DB}</br> DATABASE:${DATABASE}</br> PASS_DB:${PASS_DB}</br>PORT_DB:${PORT_DB}</br>`)
 })
